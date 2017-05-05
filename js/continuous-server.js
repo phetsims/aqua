5420703cf25339c664a8536847b724fea49371a0
Jonathan Olson <olsonsjc@gmail.com>
2017-05-05 10:33:47 -0600

    Potential workaround for https://github.com/phetsims/aqua/issues/20

--- js/continuous-server.js
+++ js/continuous-server.js
@@ -382,7 +382,7 @@ function npmUpdateRepoInfo( repoInfo, callback, errorCallback ) {
       errorCallback( 'Could not access directory for npm update: ' + dir );
     }
     else {
-      execute( NPM_CMD, [ 'update', '--cache=../npm-caches/' + repoInfo.repo ], repoInfo.base + '/' + repoInfo.repo, function( stdout, stderr ) {
+      execute( NPM_CMD, [ 'update', '--cache=../npm-caches/' + repoInfo.repo, '--tmp=../npm-tmp' ], repoInfo.base + '/' + repoInfo.repo, function( stdout, stderr ) {
         callback();
       }, function( stdout, stderr, code ) {
         errorCallback( 'Failure to npm update ' + repoInfo.base + '/' + repoInfo.repo + ':\n' + stdout + '\n' + stderr );
