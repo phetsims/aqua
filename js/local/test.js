7539b76b4ddbc0906de3c693ae18a5d9787f8ee2
samreid <reids@colorado.edu>
2018-10-09 17:59:04 -0600

    Minor cleanup, see https://github.com/phetsims/aqua/issues/52

--- js/local/test.js
+++ js/local/test.js
@@ -5,9 +5,15 @@ const testPage = require( './testPage' );
   'use strict';
 
   var x = await testPage( 'http://localhost/axon/axon-tests.html' );
+  var y = await testPage( 'http://localhost/scenery/scenery-tests.html?ea' );
   console.log( 'await complete!' );
   console.log( x );
+  console.log( y );
 
+  // var values = await Promise.all( [
+  //   testPage( 'http://localhost/axon/axon-tests.html' ),
+  //   testPage( 'http://localhost/scenery/scenery-tests.html?ea' )
+  // ]);
   // Promise.all( [ testPage ] ).then( function( values ) {
   //   console.log( values );
   // } );
