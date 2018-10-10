beed5bef6cc82363dc05c1b082ddf0b7d3a3391e
samreid <reids@colorado.edu>
2018-10-09 21:25:39 -0600

    Renamed testPage=>runUnitTests, see https://github.com/phetsims/aqua/issues/52

--- js/local/test.js
+++ js/local/test.js
@@ -1,5 +1,5 @@
 // Copyright 2018, University of Colorado Boulder
-const testPage = require( './testPage' );
+const runUnitTests = require( './runUnitTests' );
 const runPage = require( './runPage' );
 const puppeteer = require( 'puppeteer' );
 
@@ -8,7 +8,7 @@ const puppeteer = require( 'puppeteer' );
 
   const browser = await puppeteer.launch();
 
-  var x = await testPage( browser, 'http://localhost/axon/axon-tests.html' );
+  var x = await runUnitTests( browser, 'http://localhost/axon/axon-tests.html' );
   console.log( x );
   var z = await runPage( browser, 'http://localhost/faradays-law/faradays-law_en.html?brand=phet&ea&fuzzMouse' );
   console.log( z );
