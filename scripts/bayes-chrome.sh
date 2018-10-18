23b59a27bf8f416b23910ffcffee73e689fc837d
Jonathan Olson <jonathan.olson@colorado.edu>
2018-10-18 09:24:05 -0600

    Include debugging port

--- scripts/bayes-chrome.sh
+++ scripts/bayes-chrome.sh
@@ -6,5 +6,5 @@ echo "Running chrome-${1}"
 
 while [ 1 ]
 do
-  timeout 1${1}h google-chrome --headless --disable-gpu "https://bayes.colorado.edu/continuous-testing/aqua/html/continuous-loop.html?id=Bayes%20Chrome"
+  timeout 1${1}h google-chrome --headless --disable-gpu --disable-software-rasterizer --remote-debugging-port=934${1} "https://bayes.colorado.edu/continuous-testing/aqua/html/continuous-loop.html?id=Bayes%20Chrome"
 done
