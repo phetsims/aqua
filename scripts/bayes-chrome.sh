c9c10f6e3df97fe62013a4fd059ee181552c03b3
Jonathan Olson <jonathan.olson@colorado.edu>
2018-10-18 09:04:06 -0600

    Remove --disable-gpu from chrome script

--- scripts/bayes-chrome.sh
+++ scripts/bayes-chrome.sh
@@ -6,5 +6,5 @@ echo "Running chrome-${1}"
 
 while [ 1 ]
 do
-  timeout 1${1}h google-chrome --headless --disable-gpu --remote-debugging-port=922${1} "https://bayes.colorado.edu/continuous-testing/aqua/html/continuous-loop.html?id=Bayes%20Chrome"
+  timeout 1${1}h google-chrome --headless --remote-debugging-port=922${1} "https://bayes.colorado.edu/continuous-testing/aqua/html/continuous-loop.html?id=Bayes%20Chrome"
 done
