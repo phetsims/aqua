a75c1e731283bd59bcabce10b9859a23cd61969a
Jonathan Olson <jonathan.olson@colorado.edu>
2018-10-17 18:28:05 -0600

    Add bayes chrome script

---
+++ scripts/bayes-chrome.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Run with a single command-line argument, generally a number 1 through 9
+
+echo "Running chrome-${1}"
+
+while [ 1 ]
+do
+  timeout 1${1}h google-chrome --headless --disable-gpu --remote-debugging-port=922${1} "https://bayes.colorado.edu/continuous-testing/aqua/html/continuous-loop.html?id=Bayes%20Chrome"
+done
