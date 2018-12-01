e37251bae62aa935a2f929f0de5d8fc0b8e6c40b
samreid <reids@colorado.edu>
2018-11-30 22:23:31 -0700

    Parallelize tests, see https://github.com/phetsims/aqua/issues/52

--- js/local/test.js
+++ js/local/test.js
@@ -8,6 +8,11 @@ const _ = require( '../../../sherpa/lib/lodash-4.17.4.js' ); // eslint-disable-l
 ( async () => {
   'use strict';
 
+  // var seed = parseInt( process.argv[ 2 ], 10 );
+  var groups = parseInt( process.argv[ 3 ], 10 );
+  var groupIndex = parseInt( process.argv[ 4 ], 10 );
+  var command = process.argv[ 5 ];
+
   const browser = await puppeteer.launch();
   const readList = filename => fs.readFileSync( '../perennial/data/' + filename, 'utf8' ).split( '\n' ).filter( name => name.length > 0 );
 
@@ -35,22 +40,26 @@ const _ = require( '../../../sherpa/lib/lodash-4.17.4.js' ); // eslint-disable-l
   } ).map( getUnitTestURL );
 
   const timeout = 5000;
-  const tests = [];
+  const allTests = [];
   const pairs = [];
 
   // Run all unit tests
-  unitTests.forEach( test => tests.push( {
-    name: test,
-    type: 'Unit Test',
-    run: () => runUnitTests( browser, test )
-  } ) );
+  if ( command === 'UNIT' ) {
+    unitTests.forEach( test => allTests.push( {
+      name: test,
+      type: 'Unit Test',
+      run: () => runUnitTests( browser, test )
+    } ) );
+  }
 
   // Randomly pick a subset of sims to fuzz test
-  [ _.sample( testableRunnables, 1 ) ].forEach( test => tests.push( {
-    name: test,
-    type: 'Fuzz Test',
-    run: () => runPage( browser, `http://localhost/${test}/${test}_en.html?brand=phet&ea&fuzz`, timeout )
-  } ) );
+  if ( command === 'FUZZ' ) {
+    [ _.sample( testableRunnables, 1 ) ].forEach( test => allTests.push( {
+      name: test,
+      type: 'Fuzz Test',
+      run: () => runPage( browser, `http://localhost/${test}/${test}_en.html?brand=phet&ea&fuzz`, timeout )
+    } ) );
+  }
 
   // const phetioSimsToTest = ['faradays-law'];
   // phetioSimsToTest.forEach( sim => tests.push( { name: sim, type: 'Fuzz Studio', run: () => runPage( browser, `http://localhost/phet-io-wrappers/studio/?sim=${sim}&phetioDebug&fuzz`, timeout ) } ) );
@@ -60,17 +69,21 @@ const _ = require( '../../../sherpa/lib/lodash-4.17.4.js' ); // eslint-disable-l
   // const simsToTest = ['graphing-quadratics'];
   // simsToTest.forEach( test => tests.push( { name: test, type: 'Fuzz Test', run: () => runPage( browser, `http://localhost/${test}/${test}_en.html?brand=phet&ea&fuzz`, timeout ) } ) );
 
-  console.log( 'Running ' + tests.length + ' tests' );
+  // console.log( 'Found ' + allTests.length + ' tests' );
+  const tests = _.partition( allTests, test => allTests.indexOf( test ) % groups === groupIndex )[ 0 ];
+  // console.log( 'Running: ' + tests.map( _ => _.name ).join( '\n' ) );
   for ( const test of tests ) {
     const result = await test.run();
     pairs.push( { test, result } );
   }
-  const passedPairs = pairs.filter( pair => pair.result.ok );
+  // const passedPairs = pairs.filter( pair => pair.result.ok );
   const failedPairs = pairs.filter( pair => !pair.result.ok );
 
-  console.log();
-  console.log( `passed (${passedPairs.length})\n${passedPairs.map( pair => pair.test.type + ': ' + pair.test.name ).join( '\n' )}\n` );
-  console.log( `failed (${failedPairs.length})\n${failedPairs.map( pair => pair.test.type + ': ' + pair.test.name ).join( '\n' )}\n` );
+  // console.log();
+  // console.log( `passed (${passedPairs.length})\n${passedPairs.map( pair => pair.test.type + ': ' + pair.test.name ).join( '\n' )}\n` );
+  if ( failedPairs.length > 0 ) {
+    console.log( `failed (${failedPairs.length})\n${failedPairs.map( pair => pair.test.type + ': ' + pair.test.name ).join( '\n' )}\n` );
+  }
 
   await browser.close();
 } )();
--- scripts/auto-tests-local.sh
+++ scripts/auto-tests-local.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 # https://www.codeword.xyz/2015/09/02/three-ways-to-script-processes-in-parallel/
+# Run fast local tests as a sanity check before committing or pushing.
 
-node js/local/test.js &
+RAND=$RANDOM
+node js/local/test.js $RAND 3 0 UNIT &
+node js/local/test.js $RAND 3 1 UNIT &
+node js/local/test.js $RAND 3 2 UNIT &
+node js/local/test.js $RAND 1 0 FUZZ &
 grunt lint-everything --repo=faradays-law &
 
 wait
