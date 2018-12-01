ced7421a15fe3ee233b843ed51060105abf59214
samreid <reids@colorado.edu>
2018-11-30 21:40:41 -0700

    Streamline tests and report results, see https://github.com/phetsims/aqua/issues/52

--- js/local/test.js
+++ js/local/test.js
@@ -3,32 +3,16 @@ const fs = require( 'fs' );
 const puppeteer = require( 'puppeteer' );
 const runPage = require( './runPage' );
 const runUnitTests = require( './runUnitTests' );
+const _ = require( '../../../sherpa/lib/lodash-4.17.4.js' ); // eslint-disable-line
 
 ( async () => {
   'use strict';
-
-  /**
-   * Shuffles array in place.
-   * @param {Array} a - items An array containing the items.
-   */
-    // function shuffle( a ) {
-    //   var j;
-    //   var x;
-    //   var i;
-    //   for ( i = a.length - 1; i > 0; i-- ) {
-    //     j = Math.floor( Math.random() * ( i + 1 ) );
-    //     x = a[ i ];
-    //     a[ i ] = a[ j ];
-    //     a[ j ] = x;
-    //   }
-    //   return a;
-    // }
 
   const browser = await puppeteer.launch();
   const readList = filename => fs.readFileSync( '../perennial/data/' + filename, 'utf8' ).split( '\n' ).filter( name => name.length > 0 );
 
   // TODO: why do we have the "testable" lists?
-  // const testableRunnables = readList( 'testable-runnables' );
+  const testableRunnables = readList( 'testable-runnables' );
   // const testablePhetio = readList( 'testable-phet-io' );
   const activeRepos = readList( 'active-repos' );
 
@@ -46,13 +30,16 @@ const runUnitTests = require( './runUnitTests' );
   }; // TODO: support arbitrary prefix for localhost
 
   // Find repos that have qunit tests by searching for them
-  const unitTests = activeRepos.filter( repo => fs.existsSync( getUnitTestFile( repo ) ) ).map( getUnitTestURL );
+  const unitTests = activeRepos.filter( repo => {
+    return fs.existsSync( getUnitTestFile( repo ) ) && repo !== 'scenery' && repo !== 'scenery-phet';
+  } ).map( getUnitTestURL );
 
-  const timeout = 10000;
+  const timeout = 5000;
   let passed = 0;
   let failed = 0;
   var tests = [];
-  const tallyTest = result => {
+  const pairs = [];
+  const tallyTest = ( test, result ) => {
     console.log( result );
     if ( result.ok ) {
       passed++;
@@ -61,35 +48,46 @@ const runUnitTests = require( './runUnitTests' );
       failed++;
     }
     console.log( 'Passed: ' + passed + '/' + tests.length + ', Failed: ' + failed + '/' + tests.length );
+    pairs.push( { test, result } );
   };
 
-  // These are all tests
-  // @formatter:off
-  // unitTests.forEach( test => tests.push( { name: test, type: 'Unit Test', run: () => runUnitTests( browser, test ) } ) );
-  // testableRunnables.forEach( test => tests.push( { name: test, type: 'Fuzz Test', run: () => runPage( browser, `http://localhost/${test}/${test}_en.html?brand=phet&ea&fuzz`, timeout ) } ) );
-  // testablePhetio.forEach( sim => tests.push( { name: sim, type: 'Fuzz Studio', run: () => runPage( browser, `http://localhost/phet-io-wrappers/studio/?sim=${sim}&phetioDebug&fuzz`, timeout ) } ) );
-  // testablePhetio.forEach( sim => tests.push( { name: sim, type: 'Fuzz Mirror Inputs', run: () => runPage( browser, `http://localhost/phet-io-wrappers/mirror-inputs/?sim=${sim}&phetioDebug&fuzz`, timeout ) } ) );
-  // testablePhetio.forEach( sim => tests.push( { name: sim, type: 'Fuzz State', run: () => runPage( browser, `http://localhost/phet-io-wrappers/state/?sim=${sim}&phetioDebug&fuzz&numberOfMillisecondsBetweenUpdates=50`, timeout ) } ) );
-  // testablePhetio.forEach( sim => tests.push( { name: sim, type: 'PhET-iO Wrapper Tests', run: () => runPage( browser, `http://localhost/phet-io-wrappers/phet-io-wrappers-tests.html?sim=${sim}&testWrappers=false`, timeout ) } ) );
-
   // Run all unit tests
-  unitTests.forEach( test => tests.push( { name: test, type: 'Unit Test', run: () => runUnitTests( browser, test ) } ) );
-  const phetioSimsToTest = ['faradays-law'];
-  phetioSimsToTest.forEach( sim => tests.push( { name: sim, type: 'Fuzz Studio', run: () => runPage( browser, `http://localhost/phet-io-wrappers/studio/?sim=${sim}&phetioDebug&fuzz`, timeout ) } ) );
-  phetioSimsToTest.forEach( sim => tests.push( { name: sim, type: 'Fuzz Mirror Inputs', run: () => runPage( browser, `http://localhost/phet-io-wrappers/mirror-inputs/?sim=${sim}&phetioDebug&fuzz`, timeout ) } ) );
-  phetioSimsToTest.forEach( sim => tests.push( { name: sim, type: 'Fuzz State', run: () => runPage( browser, `http://localhost/phet-io-wrappers/state/?sim=${sim}&phetioDebug&fuzz&numberOfMillisecondsBetweenUpdates=50`, timeout ) } ) );
-  phetioSimsToTest.forEach( sim => tests.push( { name: sim, type: 'PhET-iO Wrapper Tests', run: () => runPage( browser, `http://localhost/phet-io-wrappers/phet-io-wrappers-tests.html?sim=${sim}&testWrappers=false`, timeout ) } ) );
-  const simsToTest = ['graphing-quadratics'];
-  simsToTest.forEach( test => tests.push( { name: test, type: 'Fuzz Test', run: () => runPage( browser, `http://localhost/${test}/${test}_en.html?brand=phet&ea&fuzz`, timeout ) } ) );
-  // @formatter:on
+  unitTests.forEach( test => tests.push( {
+    name: test,
+    type: 'Unit Test',
+    run: () => runUnitTests( browser, test )
+  } ) );
+
+  // Randomly pick a subset of sims to fuzz test
+  console.log( testableRunnables );
+  console.log( _.sample( testableRunnables, 1 ) );
+  [ _.sample( testableRunnables, 1 ) ].forEach( test => tests.push( {
+    name: test,
+    type: 'Fuzz Test',
+    run: () => runPage( browser, `http://localhost/${test}/${test}_en.html?brand=phet&ea&fuzz`, timeout )
+  } ) );
+
+  // const phetioSimsToTest = ['faradays-law'];
+  // phetioSimsToTest.forEach( sim => tests.push( { name: sim, type: 'Fuzz Studio', run: () => runPage( browser, `http://localhost/phet-io-wrappers/studio/?sim=${sim}&phetioDebug&fuzz`, timeout ) } ) );
+  // phetioSimsToTest.forEach( sim => tests.push( { name: sim, type: 'Fuzz Mirror Inputs', run: () => runPage( browser, `http://localhost/phet-io-wrappers/mirror-inputs/?sim=${sim}&phetioDebug&fuzz`, timeout ) } ) );
+  // phetioSimsToTest.forEach( sim => tests.push( { name: sim, type: 'Fuzz State', run: () => runPage( browser, `http://localhost/phet-io-wrappers/state/?sim=${sim}&phetioDebug&fuzz&numberOfMillisecondsBetweenUpdates=50`, timeout ) } ) );
+  // phetioSimsToTest.forEach( sim => tests.push( { name: sim, type: 'PhET-iO Wrapper Tests', run: () => runPage( browser, `http://localhost/phet-io-wrappers/phet-io-wrappers-tests.html?sim=${sim}&testWrappers=false`, timeout ) } ) );
+  // const simsToTest = ['graphing-quadratics'];
+  // simsToTest.forEach( test => tests.push( { name: test, type: 'Fuzz Test', run: () => runPage( browser, `http://localhost/${test}/${test}_en.html?brand=phet&ea&fuzz`, timeout ) } ) );
 
   console.log( 'enumerated ' + tests.length + ' tests' );
   // tests = shuffle( tests );
   for ( const test of tests ) {
     console.log( `Starting ${test.type}: ${test.name}` );
     const result = await test.run();
-    tallyTest( result );
+    tallyTest( test, result );
   }
+  var passedPairs = pairs.filter( pair => pair.result.ok );
+  var failedPairs = pairs.filter( pair => !pair.result.ok );
+
+  console.log();
+  console.log( `passed (${passedPairs.length})\n${passedPairs.map( pair => pair.test.type + ': ' + pair.test.name ).join( '\n' )}\n` );
+  console.log( `failed (${failedPairs.length})\n${failedPairs.map( pair => pair.test.type + ': ' + pair.test.name ).join( '\n' )}\n` );
 
   await browser.close();
 } )();
