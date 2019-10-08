1f964173abf116fe5b9ef3cd81096d34119d07b0
zepumph <michael.kauzmann@colorado.edu>
2019-10-08 13:58:21 -0800

    add puppeteerHelpCT, https://github.com/phetsims/aqua/issues/77

---
+++ js/local/puppeteerHelpCT.js
@@ -0,0 +1,32 @@
+// Copyright 2019, University of Colorado Boulder
+
+/**
+ * Launch puppeteer and point it to CT to help run tests faster
+ * @author Michael Kauzmann (PhET Interactive Simulations)
+ */
+
+'use strict';
+
+const puppeteer = require( 'puppeteer' );
+const assert = require( 'assert' );
+
+process.on( 'SIGINT', () => process.exit() );
+
+( async () => {
+
+  assert( process.argv[ 2 ], 'usage: node puppeteerHelpCT {{SOME_IDENTIFIER_HERE}}' );
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  page.on( 'console', msg => console.log( msg.text() ) );
+  page.on( 'error', msg => console.error( 'error from puppeteer', msg ) );
+  page.on( 'pageerror', msg => console.error( 'pageerror from puppeteer', msg ) );
+
+  try {
+    await page.goto( `https://bayes.colorado.edu/continuous-testing/aqua/html/continuous-loop.html?id=localPuppeteer${process.argv[ 2 ]}` );
+  }
+  catch( e ) {
+    throw  e;
+  }
+} )();
+
