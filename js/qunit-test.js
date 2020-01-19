3666f2b162ee0a854c162e969ac60460e5cc9c4a
phet-dev <phetadmin@gmail.com>
2020-01-19 03:04:30 -0700

    update copyright dates from daily grunt work

--- js/qunit-test.js
+++ js/qunit-test.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2019, University of Colorado Boulder
+// Copyright 2017-2020, University of Colorado Boulder
 
 /*
  * Runs QUnit tests in an iframe, and passes results to our parent frame (continuous-loop.html).
--- js/sim-test.js
+++ js/sim-test.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2019, University of Colorado Boulder
+// Copyright 2017-2020, University of Colorado Boulder
 
 /**
  * Runs simulation tests in an iframe, and passes results to our parent frame (continuous-loop.html).
