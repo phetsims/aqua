8984ab5877e7329f5d4bddb56e4badc95b3b8644
samreid <reids@colorado.edu>
2020-05-01 09:36:04 -0600

    Add main for running qunit tests in puppeteer, see https://github.com/phetsims/aqua/issues/90

---
+++ js/local/qunit-main.js
@@ -0,0 +1,36 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * Runs unit tests on a single repo.  If no unit tests are defined, this succeeds.
+ *
+ * USAGE:
+ * cd ${repo}
+ * node ../aqua/js/local/qunit-main.js
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
+
+const path = require( 'path' );
+const fs = require( 'fs' );
+
+// Identify the current repo
+const repo = process.cwd().split( path.sep ).pop();
+
+const puppeteer = require( 'puppeteer' );
+const puppeteerQUnit = require( './puppeteerQUnit' );
+const buildLocal = require( '../../../perennial/js/common/buildLocal' );
+
+( async () => {
+  const testFilePath = `${repo}/${repo}-tests.html`;
+  const exists = fs.existsSync( `../${testFilePath}` );
+  if ( exists ) {
+    const browser = await puppeteer.launch();
+    const result = await puppeteerQUnit( browser, `${buildLocal.localTestingURL}/${testFilePath}?ea` );
+    await browser.close();
+    console.log( `${repo}: ${result.ok}` );
+    process.exit( result.ok ? 0 : 1 );
+  }
+  else {
+    process.exit( 0 );
+  }
+} )();
