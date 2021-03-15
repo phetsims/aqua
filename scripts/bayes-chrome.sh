c4cae5ddb2e834640210e5a1ec90e4409de2f5e3
Jonathan Olson <jonathan.olson@colorado.edu>
2021-03-15 16:36:19 -0600

    Adjusting google chrome script to add logging, and stripping out the timeout so we can killall chrome for now

--- scripts/bayes-chrome.sh
+++ scripts/bayes-chrome.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Run with a single command-line argument, generally a number 1 through 9
+# Run with a single command-line argument, generally a number 01 through 99
 
 echo "Running chrome-${1}"
 
 while [ 1 ]
 do
-  timeout ${1}h google-chrome --headless --disable-gpu --disable-software-rasterizer --remote-debugging-port=934${1} --enable-precise-memory-info "https://bayes.colorado.edu/continuous-testing/aqua/html/continuous-loop.html?id=Bayes%20Chrome"
+  google-chrome --headless --disable-gpu --disable-software-rasterizer --remote-debugging-port=94${1} --enable-precise-memory-info --enable-logging=stderr --v=1 "https://bayes.colorado.edu/continuous-testing/aqua/html/continuous-loop.html?id=Bayes%20Chrome"
 done
