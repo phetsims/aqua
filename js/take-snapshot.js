04efc7cf67fd0360b288f8e1d43c052bc818fe96
phet-dev <phetadmin@gmail.com>
2021-05-20 14:49:46 -0600

    update copyright dates from daily grunt work

--- js/take-snapshot.js
+++ js/take-snapshot.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2020, University of Colorado Boulder
+// Copyright 2017-2021, University of Colorado Boulder
 
 /**
  * Runs a snapshot for a specific sim (url) with a given seed. It will send a number of events, and will record
