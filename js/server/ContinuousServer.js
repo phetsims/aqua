1c8289f677fcb495c409ff8d4cab3a455de1c59c
phet-dev <phetadmin@gmail.com>
2022-03-17 03:09:01 -0600

    update copyright dates from daily grunt work

--- js/server/ContinuousServer.js
+++ js/server/ContinuousServer.js
@@ -1,4 +1,4 @@
-// Copyright 2020-2021, University of Colorado Boulder
+// Copyright 2020-2022, University of Colorado Boulder
 
 /**
  * Coordinates continuous testing, and provides HTTP APIs for reports or clients that run browser tests.
