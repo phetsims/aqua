7e9fafd6bf410dbe9da53d9c0d930f18cb81baef
Sam Reid <reids@colorado.edu>
2022-03-29 22:47:01 -0600

    Use sourcemaps to process the stack trace, see https://github.com/phetsims/chipper/issues/1140

--- js/client/sim-test.js
+++ js/client/sim-test.js
@@ -6,6 +6,81 @@
  * @author Jonathan Olson <jonathan.olson@colorado.edu>
  */
 
+let sourceMapConsumerInitialized = false;
+
+/**
+ * Use the sourcemap of each file to output a "transpiled" stack trace
+ * @param {string} str
+ * @returns {Promise<string>}
+ */
+const transpileStacktrace = async str => {
+
+  // The transpiled stack trace is accumulated here
+  const newStackLines = [];
+
+  const stack = window.stackTraceParser.parse( str );
+  if ( stack.length === 0 ) {
+    throw new Error( 'No stack found' );
+  }
+
+  const header = str.split( '\n' ).find( line => line.trim().length > 0 );
+  newStackLines.push( header ); // TODO for REVIEWER: Should we keep or omit this?
+
+  // Iterate over each element in the stack. Use for loop because it works well with await
+  for ( let i = 0; i < stack.length; i++ ) {
+    const lineNumber = stack[ i ].lineNumber;
+    const methodName = stack[ i ].methodName;
+    const file = stack[ i ].file;
+    const column = stack[ i ].column;
+    if ( lineNumber === null || lineNumber < 1 ) {
+      newStackLines.push( `    at ${methodName || ''}` );
+    }
+    else {
+
+      // Load the text of the source file over the network
+      // TODO for REVIEWER: Should we cache these?
+      const response = await window.fetch( file );
+      const text = await response.text();
+
+      const lines = text.split( '\n' );
+
+      // The source map is in the last line
+      const lastLine = lines[ lines.length - 1 ];
+
+      // Strip the encoded sourcemap
+      const KEY = 'base64,';
+      const index = lastLine.indexOf( KEY );
+      if ( index > 0 ) {
+        const substring = lastLine.substring( index + KEY.length );
+
+        // Decode from base64
+        const a = atob( substring );
+
+        // Initialize lazily, to avoid the initialization if not necessary.
+        if ( !sourceMapConsumerInitialized ) {
+
+          // TODO for REVIEWER: Should we put this file or contents in sherpa?
+          // TODO for REVIEWER: I don't know enough about how the iframes are set up to know if this
+          // work can be shared across different instances
+          window.sourceMap.SourceMapConsumer.initialize( {
+            'lib/mappings.wasm': 'https://unpkg.com/source-map@0.7.3/lib/mappings.wasm'
+          } );
+          sourceMapConsumerInitialized = true;
+        }
+
+        const smc = await new window.sourceMap.SourceMapConsumer( a );
+        const pos = smc.originalPositionFor( { line: lineNumber, column: column } );
+        if ( pos && pos.line !== null ) {
+          newStackLines.push( `    at ${pos.name || ''} (${pos.source}:${pos.line}:${pos.column})` );
+        }
+      }
+      else {
+        newStackLines.push( `    at ${methodName || ''} (${file}:${lineNumber}:${column})` );
+      }
+    }
+  }
+  return newStackLines.join( '\n' );
+};
 
 ( () => {
   const options = QueryStringMachine.getAll( {
@@ -57,7 +132,7 @@
   const testInfo = JSON.parse( aqua.options.testInfo );
 
   // handling messages from sims
-  window.addEventListener( 'message', evt => {
+  window.addEventListener( 'message', async evt => {
     if ( typeof evt.data !== 'string' ) {
       return;
     }
@@ -70,7 +145,10 @@
       // Sent by Joist due to the postMessage* query parameters
       if ( data.type === 'continuous-test-error' ) {
         clearTimeout( timeoutID );
-        aqua.simpleFail( `${failPrefix + data.message}\n${data.stack}` );
+
+        // TODO for REVIEWER: How does this behave for built sim stacks?  Should it be skipped for those?
+        const transpiledStack = await transpileStacktrace( data.stack );
+        aqua.simpleFail( `${failPrefix + data.message}\n${transpiledStack}` );
       }
       else if ( data.type === 'continuous-test-unload' ) {
         clearTimeout( timeoutID );
