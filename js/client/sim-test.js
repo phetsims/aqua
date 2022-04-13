eb2489ebd8fa01740a5b735cde66898ddb3c0b47
phet-dev <phetadmin@gmail.com>
2022-04-13 03:14:46 -0600

    update copyright dates from daily grunt work

--- js/client/sim-test.js
+++ js/client/sim-test.js
@@ -1,4 +1,4 @@
-// Copyright 2020-2021, University of Colorado Boulder
+// Copyright 2020-2022, University of Colorado Boulder
 
 /**
  * Runs simulation tests in an iframe, and passes results to our parent frame (continuous-loop.html).
