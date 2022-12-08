3e2e419986d9249400f84df64d23888f09403ffb
phet-dev <phetadmin@gmail.com>
2022-12-08 03:36:11 -0700

    update copyright dates from daily grunt work

--- js/local/puppeteerHelpCT.js
+++ js/local/puppeteerHelpCT.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2021, University of Colorado Boulder
+// Copyright 2019-2022, University of Colorado Boulder
 
 /**
  * Launch puppeteer and point it to CT to help run tests faster
