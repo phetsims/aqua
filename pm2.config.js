f8a247a57f490c46b4d593d31f6462ff067474a1
mattpen <mape5853@colorado.edu>
2023-04-21 12:01:32 -0600

    added pm2 config for phetsims/special-ops#234

---
+++ pm2.config.js
@@ -0,0 +1,24 @@
+// Copyright 2023, University of Colorado Boulder
+
+module.exports = {
+  apps: [
+    {
+      name: 'ct-main',
+      cwd: '/data/share/phet/continuous-testing/ct-main/aqua',
+      script: 'grunt',
+      args: 'continuous-server --localCount=4'
+    },
+    {
+      name: 'ct-quick',
+      cwd: '/data/share/phet/continuous-testing/ct-quick/aqua',
+      script: 'grunt',
+      args: 'quick-server'
+    },
+    {
+      name: 'ct-chrome-clients',
+      cwd: '/data/share/phet/continuous-testing/ct-chrome-clients/aqua',
+      script: 'grunt',
+      args: 'client-server --clients=10'
+    }
+  ]
+};
