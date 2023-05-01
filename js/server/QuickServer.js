f5fb16ba3975b30a4a81fbc1d3b9e51cfb446238
Michael Kauzmann <michael.kauzmann@colorado.edu>
2023-05-01 10:40:27 -0600

    add no-zygote to API generation puppeteer instance, https://github.com/phetsims/aqua/issues/150

--- js/server/QuickServer.js
+++ js/server/QuickServer.js
@@ -113,7 +113,7 @@ class QuickServer {
         // To prevent filling up `/tmp`, see https://github.com/phetsims/aqua/issues/145
         `--user-data-dir=${process.cwd()}/../tmp/puppeteerUserData/`,
 
-        // Fork child processes directly to prevent orphaned chrome instances from lingering on bayes, https://github.com/phetsims/aqua/issues/150#issuecomment-1170140994
+        // Fork child processes directly to prevent orphaned chrome instances from lingering on sparky, https://github.com/phetsims/aqua/issues/150#issuecomment-1170140994
         '--no-zygote',
         '--no-sandbox'
       ]
--- js/server/puppeteerCTClient.js
+++ js/server/puppeteerCTClient.js
@@ -46,7 +46,7 @@ process.on( 'SIGINT', () => process.exit() );
         // To prevent filling up `/tmp`, see https://github.com/phetsims/aqua/issues/145
         `--user-data-dir=${process.cwd()}/../tmp/puppeteerUserData/`,
 
-        // Fork child processes directly to prevent orphaned chrome instances from lingering on bayes, https://github.com/phetsims/aqua/issues/150#issuecomment-1170140994
+        // Fork child processes directly to prevent orphaned chrome instances from lingering on sparky, https://github.com/phetsims/aqua/issues/150#issuecomment-1170140994
         '--no-zygote',
         '--no-sandbox'
       ]
