172d73b182b4c3633353b5ba8a98f88f8fb60f6b
Michael Kauzmann <michael.kauzmann@colorado.edu>
2023-05-15 13:59:06 -0600

    add status for outputjs when copying a new snapshot directory, https://github.com/phetsims/aqua/issues/178

--- js/server/Snapshot.js
+++ js/server/Snapshot.js
@@ -85,6 +85,7 @@ class Snapshot {
         await copyDirectory( `${this.rootDir}/${repo}`, `${this.directory}/${repo}`, {} );
       }
 
+      this.setStatus( 'Running output-js-all on snapshot' );
       await execute( gruntCommand, [ 'output-js-all' ], `${this.directory}/chipper` );
     }
 
