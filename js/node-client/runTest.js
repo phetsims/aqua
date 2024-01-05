c2414cb3a4095ec92ee4cad35547affdbc122cac
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-01-05 15:56:20 -0700

    runTest should use browserPageLoad, https://github.com/phetsims/aqua/issues/198

--- js/node-client/runTest.js
+++ js/node-client/runTest.js
@@ -9,9 +9,9 @@
 const _ = require( 'lodash' );
 const sendTestResult = require( './sendTestResult' );
 const puppeteer = require( '../../../perennial/node_modules/puppeteer' );
+const browserPageLoad = require( '../../../perennial/js/common/browserPageLoad' );
 const winston = require( 'winston' );
 const path = require( 'path' );
-const sleep = require( '../../../perennial/js/common/sleep' );
 require( 'dotenv' ).config();
 
 /**
@@ -19,19 +19,114 @@ require( 'dotenv' ).config();
  * @public
  *
  * @param {Object} testInfo
- * @param {Object} [options]
+ * @param {Object} [options] - see browserPageLoad
  * @returns {Promise}
  */
 module.exports = async function( testInfo, options ) {
+
+  // Make sure not to resolve if we still have results being sent.
+  let currentSendingCount = 0;
+
+  let receivedPassFail = false; // When received a test result
+  let gotNextTest = false; // When received the next-test command.
+
+  const allowedTimeToLoad = 400000;
+
+  // The whole log of the browser run. Keep this as one string to send it as a CT result.
+  let log = '';
+
   options = _.merge( {
     fileServerURL: 'https://sparky.colorado.edu/continuous-testing', // {string} - The server to use
     browserCreator: puppeteer,
-    browser: null, // If provided, browserCreator is not used to create a browser, and this browser is not closed.
 
-    launchOptions: {
-      args: [
-        '--disable-gpu'
-      ]
+    gotoTimeout: 280000,
+    allowedTimeToLoad: allowedTimeToLoad,
+    rejectErrors: false,
+    rejectPageErrors: false,
+
+    // We will resolve ourselves
+    waitAfterLoad: 0,
+    resolveFromLoad: false,
+
+    logConsoleOutput: true,
+    logNavigation: true,
+
+    // Fixed https://github.com/phetsims/aqua/issues/191
+    cachePages: false,
+
+    // Keep track of all messages to send them out with our CT reporting
+    logger: message => {
+      winston.info( message );
+      log += `${message}\n`;
+    },
+    onLoadTimeout: ( resolve, reject ) => {
+      if ( !gotNextTest ) { // If we have gotten this signal, then we are already handling the resolving.
+        if ( receivedPassFail && currentSendingCount === 0 ) {
+          resolve();
+        }
+        else {
+          reject( new Error( `Did not get next-test message in ${allowedTimeToLoad}ms (currentSendingCount ${currentSendingCount}): ${JSON.stringify( testInfo.test )}` ) );
+        }
+      }
+    },
+
+    evaluateOnNewDocument: () => {
+      const oldParent = window.parent;
+
+      window.parent = {
+        postMessage: event => {
+          window.onPostMessageReceived && window.onPostMessageReceived( event );
+          oldParent && oldParent.postMessage( event, '*' );
+        }
+      };
+    },
+
+    onPageCreation: async ( page, resolve ) => {
+
+      const resolveIfReady = () => {
+        winston.debug( `resolveIfReady, currentSendingCount:${currentSendingCount}, gotNextTest:${gotNextTest}` );
+        if ( currentSendingCount === 0 && gotNextTest ) {
+          winston.debug( 'resolving from test message' );
+          resolve();
+        }
+      };
+
+      // Define a window.onMessageReceivedEvent function on the page.
+      await page.exposeFunction( 'onPostMessageReceived', async event => {
+        try {
+          event = JSON.parse( event );
+        }
+        catch( e ) {
+          return;
+        }
+
+        if ( event.type === 'test-pass' ) {
+          receivedPassFail = true;
+          winston.info( 'Sending PASS result' );
+
+          currentSendingCount++;
+          const serverMessage = await sendTestResult( event.message, testInfo, true, options );
+          currentSendingCount--;
+
+          winston.info( `Server receipt: ${JSON.stringify( serverMessage )}` );
+          resolveIfReady();
+        }
+        else if ( event.type === 'test-fail' ) {
+          receivedPassFail = true;
+          winston.info( 'Sending FAIL result' );
+
+          currentSendingCount++;
+          const serverMessage = await sendTestResult( `${event.message}\n${log}`, testInfo, false, options );
+          currentSendingCount--;
+
+          winston.info( `Server receipt: ${JSON.stringify( serverMessage )}` );
+          resolveIfReady();
+        }
+        else if ( event.type === 'test-next' ) {
+          gotNextTest = true;
+          resolveIfReady();
+        }
+      } );
     }
   }, options );
 
@@ -59,9 +154,6 @@ module.exports = async function( testInfo, options ) {
     }, options.launchOptions );
   }
 
-  const majorTimeout = 280000;
-  const bailTimout = 400000;
-
   const testInfoQueryParam = `testInfo=${encodeURIComponent( JSON.stringify( {
     test: testInfo.test,
     snapshotName: testInfo.snapshotName,
@@ -70,162 +162,10 @@ module.exports = async function( testInfo, options ) {
 
   const url = `${options.fileServerURL}/aqua/html/${testInfo.url}${testInfo.url.includes( '?' ) ? '&' : '?'}${testInfoQueryParam}`;
 
-  const ownsBrowser = !options.browser;
-
-  let browser;
-  let page;
-  let log = '';
-
-  // Gets included in any error/fail messages
-  const logResult = message => {
-    winston.info( message );
-    log += `${message}\n`;
-  };
-
   try {
-    browser = options.browser || await options.browserCreator.launch( options.launchOptions );
-
-    page = await browser.newPage();
-
-    page.setCacheEnabled( false ); // For working on https://github.com/phetsims/aqua/issues/191
-
-    await page.setDefaultNavigationTimeout( majorTimeout );
-
-    // The API for playwright was much more complicated, so just support puppeteer
-    if ( !options.browser && options.browserCreator === puppeteer &&
-         process.env.BASIC_PASSWORD && process.env.BASIC_USERNAME ) {
-      await page.authenticate( { username: process.env.BASIC_USERNAME, password: process.env.BASIC_PASSWORD } );
-    }
-
-    let receivedPassFail = false;
-    let gotNextTest = false;
-
-    // promote for use outside the closure
-    let resolve;
-    let reject;
-    const promise = new Promise( ( res, rej ) => {
-      resolve = res;
-      reject = rej;
-    } );
-
-    // Make sure not to resolve if we still have results being sent.
-    let currentSendingCount = 0;
-
-    const resolveIfReady = () => {
-      if ( currentSendingCount === 0 && gotNextTest ) {
-        resolve();
-      }
-    };
-
-    // Define a window.onMessageReceivedEvent function on the page.
-    await page.exposeFunction( 'onPostMessageReceived', async event => {
-      try {
-        event = JSON.parse( event );
-      }
-      catch( e ) {
-        return;
-      }
-
-      if ( event.type === 'test-pass' ) {
-        receivedPassFail = true;
-        winston.info( 'Sending PASS result' );
-
-        currentSendingCount++;
-        const serverMessage = await sendTestResult( event.message, testInfo, true, options );
-        currentSendingCount--;
-
-        winston.info( `Server receipt: ${JSON.stringify( serverMessage )}` );
-        resolveIfReady();
-      }
-      else if ( event.type === 'test-fail' ) {
-        receivedPassFail = true;
-        winston.info( 'Sending FAIL result' );
-
-        currentSendingCount++;
-        const serverMessage = await sendTestResult( `${event.message}\n${log}`, testInfo, false, options );
-        currentSendingCount--;
-
-        winston.info( `Server receipt: ${JSON.stringify( serverMessage )}` );
-        resolveIfReady();
-      }
-      else if ( event.type === 'test-next' ) {
-        gotNextTest = true;
-        resolveIfReady();
-      }
-    } );
-
-    // Support puppeteer (evaluateOnNewDocument) or playwright (addInitScript)
-    await ( ( page.evaluateOnNewDocument || page.addInitScript ).call( page, () => {
-      const oldParent = window.parent;
-
-      window.parent = {
-        postMessage: event => {
-          window.onPostMessageReceived && window.onPostMessageReceived( event );
-          oldParent && oldParent.postMessage( event, '*' );
-        }
-      };
-    } ) );
-
-    page.on( 'response', async response => {
-      // 200 and 300 class status are most likely fine here
-      if ( response.url() === url && response.status() >= 400 ) {
-        logResult( `[ERROR] Could not load from status: ${response.status()}` );
-      }
-    } );
-    page.on( 'console', msg => {
-      let messageTxt = msg.text();
-
-      // Append the location to messages that would benefit from it.
-      if ( messageTxt.includes( 'net:' ) || messageTxt.includes( 'Failed to load resource' ) ) {
-        messageTxt += `: \t ${msg.location().url}`;
-      }
-      logResult( `[CONSOLE] ${messageTxt}` );
-    } );
-
-    page.on( 'error', message => {
-      logResult( `[ERROR] ${message}` );
-    } );
-    page.on( 'pageerror', message => {
-      logResult( `[PAGE ERROR] ${message}` );
-    } );
-    page.on( 'framenavigated', frame => {
-      logResult( `[NAVIGATED] ${frame.url()}` );
-    } );
-
-    // Run asynchronously
-    ( async () => {
-      await sleep( bailTimout );
-      if ( !gotNextTest ) {
-        if ( receivedPassFail && currentSendingCount === 0 ) {
-          resolve();
-        }
-        else {
-          reject( new Error( `Did not get next-test message in ${bailTimout}ms (currentSendingCount ${currentSendingCount}): ${JSON.stringify( testInfo.test )}` ) );
-        }
-      }
-    } )();
-
-    logResult( `[URL] ${url}` );
-
-    await Promise.all( [
-      page.goto( url, {
-        timeout: majorTimeout
-      } ),
-      promise
-    ] );
-    winston.debug( 'promise resolved' );
-
-    !page.isClosed() && await page.close();
-    winston.debug( 'page closed' );
-
-    // If we created a temporary browser, close it
-    ownsBrowser && await browser.close();
-    winston.debug( 'browser closed' );
+    await browserPageLoad( options.browserCreator, url, options );
   }
-
   catch( e ) {
-    page && !page.isClosed() && await page.close();
-    ownsBrowser && browser && await browser.close();
-    throw new Error( `${e}\n${log}` );
+    throw new Error( `${e}\n${log}` ); // Post the error with the log from the browser run
   }
 };
