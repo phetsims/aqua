0b4a613cdcfa76df9ee9ca62bc7e0b25bc4bca20
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-04-01 12:55:26 -0600

    move browser tools to a directory where they are linted as browser (not Nodejs), https://github.com/phetsims/aqua/issues/208
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

---
+++ js/browser-tools/.eslintrc.js
@@ -0,0 +1,11 @@
+// Copyright 2022-2023, University of Colorado Boulder
+// @author Michael Kauzmann
+
+
+module.exports = {
+  extends: '../../../chipper/eslint/phet-library_eslintrc.js',
+
+  rules: {
+    'bad-sim-text': 'off'
+  }
+};
